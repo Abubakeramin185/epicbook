dee1f2833622fc856ea4ebb72c31dd1e7dd079b9
Abubakeramin185 <abubakeramin185@gmail.com>
2025-04-07 08:17:05 +0200

    invia

---
+++ my-app-epicbooks/src/Components/CommentList.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+
+export default function CommentList (){
+    return(
+        <div className="commentlist">
+            <ul>
+                <li>commenti di utenti</li>
+                
+            </ul>
+        </div>
+    )
+}
